297d296ab1c31164eb2bbcb7be8893ab0e1ee4ae
vinaypuppal <me@vinaypuppal.com>
2020-09-05 06:00:36 +0530

    initial setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main className="flex items-center justify-center w-full min-h-screen text-black bg-white dark:bg-black dark:text-white">
+      <h1 className="text-2xl sm:text-5xl md:text-6xl">Hello from Preact + Next.js</h1>
+    </main>
+  );
+};
+
+export default Home;
