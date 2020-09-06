6c224feb3997fb7e6677552681e356f241a90a15
vinaypuppal <me@vinaypuppal.com>
2020-09-07 00:49:54 +0530

    configure netlify

--- next.config.js
+++ next.config.js
@@ -3,29 +3,29 @@ const withPrefresh = require('@prefresh/next');
 const config = {
   experimental: {
     modern: true,
-    polyfillsOptimization: true
+    polyfillsOptimization: true,
   },
+  target: 'serverless',
 
   webpack(config, { dev, isServer }) {
-    const splitChunks = config.optimization && config.optimization.splitChunks
+    const splitChunks = config.optimization && config.optimization.splitChunks;
     if (splitChunks) {
       const cacheGroups = splitChunks.cacheGroups;
       const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
       if (cacheGroups.framework) {
         cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
-          test: preactModules
+          test: preactModules,
         });
         cacheGroups.commons.name = 'framework';
-      }
-      else {
+      } else {
         cacheGroups.preact = {
           name: 'commons',
           chunks: 'all',
-          test: preactModules
+          test: preactModules,
         };
       }
     }
-    
+
     // Install webpack aliases:
     const aliases = config.resolve.alias || (config.resolve.alias = {});
     aliases.react = aliases['react-dom'] = 'preact/compat';
@@ -33,14 +33,15 @@ const config = {
     // inject Preact DevTools
     if (dev && !isServer) {
       const entry = config.entry;
-      config.entry = () => entry().then(entries => {
-        entries['main.js'] = ['preact/debug'].concat(entries['main.js'] || []);
-        return entries;
-      });
+      config.entry = () =>
+        entry().then((entries) => {
+          entries['main.js'] = ['preact/debug'].concat(entries['main.js'] || []);
+          return entries;
+        });
     }
 
     return config;
-  }
+  },
 };
 
 module.exports = withPrefresh(config);
