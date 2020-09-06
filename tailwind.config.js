e308baffd1eb31f67c2e3be7f153673b2172b57d
vinaypuppal <me@vinaypuppal.com>
2020-09-06 08:01:00 +0530

    install some deps and update configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   future: {
     removeDeprecatedGapUtilities: true,
@@ -6,7 +8,22 @@ module.exports = {
   experimental: 'all',
   purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        display: ['Gilroy', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        grape: '#A65FEC',
+      },
+      opacity: {
+        10: '0.1',
+        12: '0.12',
+        20: '0.2',
+        30: '0.3',
+        40: '0.4',
+      },
+    },
   },
   variants: {},
   plugins: [],
