d7b6c384d294ac7cea55eeb448487c6a0c7f4ceb
vinaypuppal <me@vinaypuppal.com>
2020-09-08 00:18:25 +0530

    move lang to document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { DefaultSeo } from 'next-seo';
-import Head from 'next/head';
 
 import '../styles/index.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <html lang="en" />
-      </Head>
       <DefaultSeo title="Infinite Gallery" description="Infinite Gallery using unsplash image API" />
       <Component {...pageProps} />
     </>
