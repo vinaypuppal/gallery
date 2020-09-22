603c12221924bd0b885465ef193569f2b46187e1
vinaypuppal <me@vinaypuppal.com>
2020-09-23 01:25:02 +0530

    add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useAnalytics } from '@happykit/analytics';
 import { DefaultSeo } from 'next-seo';
 import Head from 'next/head';
 
@@ -7,6 +8,17 @@ import '@reach/combobox/styles.css';
 import '@snackbar/core/dist/snackbar.min.css';
 
 function MyApp({ Component, pageProps }) {
+  useAnalytics({
+    publicKey: 'analytics_pub_e3ac1fb78a',
+
+    // skipHostnames usually defaults to ["localhost"], but
+    // it seems as if you haven't deployed your project yet.
+    // So we enable tracking page views on localhost.
+    // This way you can see what HappyKit is like.
+    //
+    // Delete this setting once you've deployed your app.
+    skipHostnames: [],
+  });
   return (
     <>
       <DefaultSeo title="Infinite Gallery" description="Infinite Gallery using unsplash image API" />
