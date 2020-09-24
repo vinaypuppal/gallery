8161a30a669c4700edb23ef287b456b1a90f57ad
vinaypuppal <me@vinaypuppal.com>
2020-09-24 23:35:11 +0530

    update perPage to 9

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 export const config = {
   unsplashAccessKey: process.env.NEXT_PUBLIC_UNSPLASH_KEY,
-  perPage: 14,
+  perPage: 9,
   columnsMediaQueries: ['(min-width: 768px)', '(min-width: 640px)'],
   columns: [3, 2],
   defaultColumns: 2,
